07bb2e7fc08d070348d2365ed333fcd2dbd06c4a
thePeaDream <PeaDream@outlook.com>
2024-06-27 00:02:25 +0800

    管道访问控制

--- 管道/pipeline.cpp
+++ 管道/pipeline.cpp
@@ -31,17 +31,30 @@ int main()
         //读取要有一个缓冲区，用来保存读到的数据
         char buffer[1024];
         memset(buffer,0,sizeof buffer);
+        
+        //如果父进程没有关闭写端，子进程也不能关闭读端，要读取或等待父进程写入
+        //如果父进程关闭写端，子进程也读完了管道的内容，此时read会返回0，说明读到了文件结尾
+        sleep(8);
         while(true)
-        {
+        {      
             ssize_t num = read(pipefd[0],buffer,sizeof(buffer) - 1);
             if(num > 0)
             {
                 buffer[num] = '\0';
                 std::cout << "子进程pid:"<< getpid() <<"读取父进程的数据:" << buffer << std::endl;
+                //测试读端先关闭
+                //close(pipefd[0]);
+                //exit(0);
+            }
+            if(num == 0)
+            {
+                std::cout << "写入端取消fd,读取完成！！！！！" << std::endl;
+                break;
             }
         }
         //通信完毕
         close(pipefd[0]);
+        exit(0);
     }
     else if(id > 0)
     {
@@ -58,14 +71,21 @@ int main()
             snprintf(sent_buffer,sizeof(sent_buffer),"%s : %d, 父进程pid是%d",str.c_str(),count++,getpid());
             //将sent_buffer写入到管道
             write(pipefd[1],sent_buffer,strlen(sent_buffer));
-            sleep(2);
+            //sleep(1);
+            if(count == 5)
+            {
+                //父进程不写了
+                break;
+            }
+           //如果读端先关闭，os会自动终止写端进程
         }
+         //通信完毕
+        close(pipefd[1]);
         //阻塞等待
         pid_t ret = waitpid(id,nullptr,0);
+        //测试读端先关闭，写端会被os终止
+        std::cout << "aaa" << std::endl;
         assert(ret);
-
-        //通信完毕
-        close(pipefd[1]);
     }
     return 0;
 }
